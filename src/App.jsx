a81821a7a90166500c9d722ea8b9978537688167
suleman-084 <sallusk084@gmail.com>
2025-11-22 02:35:26 +0530

    chunked the unnecessary files and created the router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import Body from './components/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <div className="min-h-screen bg-green-300 flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Tailwind Background Test ✔️
-      </h1>
-    </div>
+<Body />
   )
 }
 
